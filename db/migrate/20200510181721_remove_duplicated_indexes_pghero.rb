b111ccf9f59af05dbb951dabe2746f9a9b9ccf7a
Starfall <admin@plural.cafe>
2020-07-01 13:52:32 -0500

    fix db migration trying to remove a nonexistent index

--- db/migrate/20200510181721_remove_duplicated_indexes_pghero.rb
+++ db/migrate/20200510181721_remove_duplicated_indexes_pghero.rb
@@ -5,7 +5,7 @@ class RemoveDuplicatedIndexesPghero < ActiveRecord::Migration[5.2]
     remove_index :account_pins, name: "index_account_pins_on_account_id", column: :account_id
     remove_index :announcement_mutes, name: "index_announcement_mutes_on_account_id", column: :account_id
     remove_index :announcement_reactions, name: "index_announcement_reactions_on_account_id", column: :account_id
-    remove_index :bookmarks, name: "index_bookmarks_on_account_id", column: :account_id
+    # remove_index :bookmarks, name: "index_bookmarks_on_account_id", column: :account_id
     remove_index :markers, name: "index_markers_on_user_id", column: :user_id
   end
 end
