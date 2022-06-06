bd872c93333b90f85d90bc60f5690537bee15ee2
Starfall <us@starfall.systems>
2022-06-06 14:04:52 -0500

    build.sh: detect if starting dockerd is hanging

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env -S zsh -uo pipefail
+
 commit=`git rev-parse HEAD | cut -c -7`
-echo $commit
+echo "Building commit $commit"
 
 yarn install
-systemctl start docker
+
+if ! systemctl is-active docker &>/dev/null; then
+	timeout 5s systemctl start docker
+	if [ $? -eq 124 ]; then
+		echo 'Docker start may be hanging... consider manual intervention:'
+		echo '  rm -rf /var/run/docker'
+		echo '  rm -f /var/run/docker.sock'
+		echo '  systemctl start docker docker.socket'
+		exit 124
+	fi
+fi
+
 docker build --build-arg SOURCE_COMMIT=$commit --tag pluralcafe/mastodon:edge .
 
 echo 'Docker image built. Push to Docker Hub with `docker push pluralcafe/mastodon:edge`.'
